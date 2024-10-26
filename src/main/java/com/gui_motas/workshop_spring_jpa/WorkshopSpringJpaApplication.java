68973e9041e418020a036d8d552d4cb7bb70de1a
gui-motas <monjogui@gmail.com>
2024-10-26 11:17:12 -0300

    Projeto criado

---
+++ src/main/java/com/gui_motas/workshop_spring_jpa/WorkshopSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gui_motas.workshop_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringJpaApplication.class, args);
+	}
+
+}
