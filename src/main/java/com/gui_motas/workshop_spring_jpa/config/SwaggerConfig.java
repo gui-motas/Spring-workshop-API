6ff872ef6de5786eefcc65c3eb8e3c4effb6300a
gui-motas <monjogui@gmail.com>
2024-11-02 20:28:08 -0300

    Adicionada documentação e testes com OpenAPIs e SwaggerUI

---
+++ src/main/java/com/gui_motas/workshop_spring_jpa/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.gui_motas.workshop_spring_jpa.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Configuration
+@Controller
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API REST - Spring Boot com JPA")
+                        .version("1.0.0")
+                        .description("Este projeto é uma API RESTful construída com Spring Boot, seguindo uma arquitetura baseada em camadas para organizar as responsabilidades do sistema. A aplicação permite operações básicas de CRUD (Create, Read, Update, Delete) e utiliza PostgreSQL para persistência dos dados."));
+    }
+
+    @GetMapping("/")
+    public RedirectView redirectToSwaggerUi() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
+
+}
